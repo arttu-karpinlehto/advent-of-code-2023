ab3273a4678c2f998a60f7572531376262debf89
Arttu Kärpinlehto <arttu@karpinlehto.fi>
2023-12-04 13:52:51 +0200

    Added day 4.

--- unit_tests.h
+++ unit_tests.h
@@ -65,3 +65,21 @@ void test_day03() {
 	input.seekg(0);	// reset and rewind the stream
 	assert(467835 == day03(2, input));
 }
+
+
+void test_day04() {
+	std::stringstream input;
+	input <<
+"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n\
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+	assert(13 == day04(1, input));
+
+	input.clear();
+	input.seekg(0);	// reset and rewind the stream
+	assert(30 == day04(2, input));
+	assert(false);
+}
